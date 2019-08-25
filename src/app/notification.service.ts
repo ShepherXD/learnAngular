dbb83a1b17dbfe077e270b01c3758afb29e7e026
shepherXD <1046019173@qq.com>
2019-08-25 21:26:51 +0800

    修复了手机端报错的问题

--- src/app/notification.service.ts
+++ src/app/notification.service.ts
@@ -5,20 +5,24 @@ import { Injectable } from '@angular/core';
 })
 export class NotificationService {
   requestNotify(): void {
-    Notification.requestPermission(() => {
-      if (Notification.permission === 'denied' ) {
-        alert(`不允许的话无法显示通知噢qaq \n
+    if (Notification !== undefined) {
+      Notification.requestPermission(() => {
+        if (Notification.permission === 'denied' ) {
+          alert(`不允许的话无法显示通知噢qaq \n
                请点击网址栏最左按钮设置通知为允许`);
-      }
-    });
+        }
+      });
+    }
   }
   notify(title: string, txt: string): void {
-    if (Notification.permission === 'granted') {
-      const notification = new Notification(title, {
-        body: txt
-      });
-    } else {
-      this.requestNotify();
+    if (Notification !== undefined) {
+      if (Notification.permission === 'granted') {
+        const notification = new Notification(title, {
+          body: txt
+        });
+      } else {
+        this.requestNotify();
+      }
     }
   }
 
